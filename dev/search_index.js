136327c7c137ae5f1d735ca393ceee5091439718
Documenter.jl <documenter@juliadocs.github.io>
2021-04-22 18:04:08 +0000

    build based on 3a4f89c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BlockSparseArrays.jl","page":"BlockSparseArrays.jl","title":"BlockSparseArrays.jl","text":"","category":"section"},{"location":"","page":"BlockSparseArrays.jl","title":"BlockSparseArrays.jl","text":"The BlockSparseArrays.jl package provides block-sparse vectors and matrices. Different from BlockSparseMatrics.jl, this package here encodes the block size in the type, which can create more efficient code.","category":"page"}]
+}
